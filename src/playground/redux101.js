17cae582ed06dc099cc27950c6fedd9cc3f5ce58
Emuohwo <warriisaac@outlook.com>
2020-04-12 13:50:33 -0700

    Dispatch action with Redux

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,7 +1,40 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = {count: 0}) => {
-    return state;
+const store = createStore((state = {count: 0}, action) => {
+    console.log('running dispatch');
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            }
+            break;
+        case 'DECREMENT':
+            return {
+                count: state.count-1
+            }
+            break;
+        case 'RESET':
+            return {
+                count: 0
+            }
+            break;
+        default:
+            return state;
+            break;
+    }
+    
 });
+
+console.log(store.getState());
+
+store.dispatch({
+    type: 'INCREMENT'
+})
+store.dispatch({
+    type: 'RESET'
+})
+store.dispatch({
+    type: 'DECREMENT'
+})
 
 console.log(store.getState());
