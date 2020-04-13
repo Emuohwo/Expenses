e2549161585c0363b55ed911adaf44a6ff423286
Emuohwo <warriisaac@outlook.com>
2020-04-13 13:17:25 -0700

    Use reducer

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -24,7 +24,7 @@ const setCount = ({ count} = {}) => ({
     count
 })
 
-const store = createStore((state = {count: 0}, action) => {
+const countReducer = ((state = {count: 0}, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
@@ -52,6 +52,8 @@ const store = createStore((state = {count: 0}, action) => {
     }
     
 });
+
+const store = createStore(countReducer)
 
 store.subscribe(() => {
     console.log(store.getState());
