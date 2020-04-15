343889f661b579e6071103ce1afadcc780164b11
Emuohwo <warriisaac@outlook.com>
2020-04-15 06:12:01 -0700

    Render Individual expense on home page

--- src/app.js
+++ src/app.js
@@ -12,12 +12,12 @@ import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill'}))
+store.dispatch(addExpense({ description: 'Water Bill', amount: 4500}))
 store.dispatch(addExpense({ description: 'Gas Bill'}));
 store.dispatch(setTextFilter('water'))
 
 setTimeout(() => {
-    store.dispatch(setTextFilter('rent'))
+    store.dispatch(setTextFilter('bill'))
 }, 3000);
 
     const state = store.getState()
