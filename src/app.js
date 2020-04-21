91242a4d4add1a211cdb46cd51eb35cbe2eb1a4b
Emuohwo <warriisaac@outlook.com>
2020-04-21 01:34:21 -0700

    skip master4 & remove dummy data

--- src/app.js
+++ src/app.js
@@ -5,25 +5,9 @@ import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
-
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water Bill', amount: 4500}))
-store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000}));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500}))
-// store.dispatch(setTextFilter('water'))
-
-// setTimeout(() => {
-//     store.dispatch(setTextFilter('bill'))
-// }, 3000);
-
-    const state = store.getState()
-    const visibleExpense = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpense);
 
 const jsx =  (
     <Provider store={store}>
