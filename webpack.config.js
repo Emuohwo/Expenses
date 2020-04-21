a67f99d9abeebb96fd16a7170a86491872321d25
Emuohwo <warriisaac@outlook.com>
2020-04-21 03:03:24 -0700

    Set Webpack for Production

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,61 @@
 const path = require('path');
 
+module.exports = (env) => {
+    console.log('env', env);
+    const isProduction = env === 'production'
+    
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
 
-
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/i,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
     }
 }
+
+// module.exports = {
+//     entry: './src/app.js',
+//     output: {
+//         path: path.join(__dirname, 'public'),
+//         filename: 'bundle.js'
+//     },
+//     module: {
+//         rules: [{
+//             loader: 'babel-loader',
+//             test: /\.js$/,
+//             exclude: /node_modules/
+//         }, {
+//             test: /\.s?css$/i,
+//             use: [
+//                 'style-loader',
+//                 'css-loader',
+//                 'sass-loader'
+//             ]
+//         }]
+//     },
+//     devtool: 'cheap-module-eval-source-map',
+//     devServer: {
+//         contentBase: path.join(__dirname, 'public'),
+//         historyApiFallback: true
+//     }
+// }
